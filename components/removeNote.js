6df2a83bc2336e889491752bd9c77138a139f0c2
Marvin <marvin.kleiner1910@gmail.com>
2024-02-14 16:31:29 +0100

    Finished Day #2 on the TODO Project

--- components/removeNote.js
+++ components/removeNote.js
@@ -1,14 +1,14 @@
 import PocketBase from 'pocketbase';
 
-export function removeNote(element, item) {
+export function removeNote(element) {
   element.addEventListener("delete", (e) => {
     e.preventDefault();
-    create(title, note);
+    create(e.target);
     // handle submit
   });
 }
 async function create(item) {
   const pb = new PocketBase("http://localhost:8090/");
-  await pb.collection("notes").delete(item.id);
+  await pb.collection("notes").delete(item);
   location.reload();
 }
