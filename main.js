1cd9c268de71c40534047a4cc2fbe38a9861564f
Marvin <marvin.kleiner1910@gmail.com>
2024-02-16 15:36:12 +0100

    GitIGnore

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import "./style/pico.min.css";
 import "./style/style.css";
-import { setupNote } from "./components/create.js";
-import modal from "./components/modal.js";
+import { setupNote } from "./components/notes/create.js";
+import modal from "./components/notes/modal.js";
 import PocketBase from "pocketbase";
-import { removeNote } from "./components/removeNote.js";
-import { updateNote } from "./components/updateNote.js";
+import { removeNote } from "./components/notes/removeNote.js";
+import { updateNote } from "./components/notes/updateNote.js";
 
 const pb = new PocketBase("http://localhost:8090/");
 let list = [];
