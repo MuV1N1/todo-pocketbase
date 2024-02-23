bce6dca5adcd0f91d728dbe73ffa84f81f910b77
Marvin <marvin.kleiner1910@gmail.com>
2024-02-23 08:32:39 +0100

    s

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style/pico.min.css";
 import "./style/style.css";
 import { setupNote } from "./components/notes/create.js";
 import modal from "./components/modal.js";
-import PocketBase from "./pocketbase";
+import PocketBase from "./pocketbase/pocketbase";
 
 import { removeNote } from "./components/notes/removeNote.js";
 import { updateNote } from "./components/notes/updateNote.js";
